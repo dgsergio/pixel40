7cf461fcd7af94a9ff7994d623e731cdac0701c2
Sergio Garofalo <dg.sergio@gmail.com>
2023-08-15 10:08:51 +0200

    feat: price updated

--- store/prices.js
+++ store/prices.js
@@ -1,18 +1,18 @@
 // 24/04/2023 blue:442-5.5%
+// 15/08/2023 *575
 const prices = {
-    usd: {
-        modulo: 34,
-        cursoStriked: 85,
-        curso: 43,
-        clase: 19.99,
-        clases: 64
-    },
-    ars: {
-        modulo: 14200,
-        cursoStriked: 35900,
-        curso: 17900,
-    },
-
-}
+  usd: {
+    modulo: 34,
+    cursoStriked: 85,
+    curso: 43,
+    clase: 19.99,
+    clases: 64,
+  },
+  ars: {
+    modulo: 20100,
+    cursoStriked: 48900,
+    curso: 24700,
+  },
+};
 
 export default prices;
