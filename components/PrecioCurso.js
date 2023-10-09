2e4c3dab16e019fc5ac4ac719cc94743ffc37553
Sergio Garofalo <dg.sergio@gmail.com>
2023-10-09 09:31:59 +0100

    feat: no more ars currency payments

--- components/PrecioCurso.js
+++ components/PrecioCurso.js
@@ -1,28 +1,21 @@
-import Modal from "./UI/Modal";
-import classes from "./PrecioCurso.module.css";
-import FormasDePago from "./layout/sections/FormasDePago";
-import PrecioCursoUsd from "./PrecioCursoUsd";
-import PrecioCursoArs from "./PrecioCursoArs";
-import { useState } from "react";
+import Modal from './UI/Modal';
+import classes from './PrecioCurso.module.css';
+import FormasDePago from './layout/sections/FormasDePago';
+import PrecioCursoUsd from './PrecioCursoUsd';
+import PrecioCursoArs from './PrecioCursoArs';
+import { useState } from 'react';
 
 const PrecioCurso = () => {
-  const [currency, setCurrency] = useState("usd");
+  const [currency, setCurrency] = useState('usd');
   const submitHandler = (e) => {
-    if (e.target.value !== "ars" && e.target.value !== "usd") return;
+    if (e.target.value !== 'ars' && e.target.value !== 'usd') return;
     setCurrency(e.target.value);
   };
 
   return (
     <Modal>
-      <div className={classes.region}>
-        <select id="region" onChange={submitHandler}>
-          <option disabled>Selecciona tu región</option>
-          <option value="usd">Internacional</option>
-          <option value="ars"> Argentina</option>
-        </select>
-      </div>
       <div className={classes.packs}>
-        {currency === "usd" ? <PrecioCursoUsd /> : <PrecioCursoArs />}
+        {currency === 'usd' ? <PrecioCursoUsd /> : <PrecioCursoArs />}
       </div>
 
       <div className={classes.payment}>Formas de Pago</div>
